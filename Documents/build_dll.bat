5a97bc2f8acaeac97d14878e1b2387f0ae6667f4
Sic <stdsic9900@naver.com>
2025-02-27 18:25:27 +0900

    최종 버전 셋업 파일 업로드

--- Documents/build_dll.bat
+++
@@ -1,2 +0,0 @@
-g++ -c -o MyApiDll.o MyApiDll.cpp -mwindows -D MYAPIDLL_EXPORTS
-g++ -o MyApiDll.dll MyApiDll.o -mwindows -s -shared -Wl,--subsystem,windows
--- Documents/build_exe.bat
+++
@@ -1,2 +0,0 @@
-g++ -c -o main.o main.cpp -mwindows
-g++ -o ColorFromPoint.exe -s main.o -mwindows -L. -lMyApiDll
